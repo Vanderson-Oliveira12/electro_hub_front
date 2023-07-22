92b1500027076c66d3f8e3be2b5ed6918648eb4d
Vanderson-Oliveira12 <vanderson.doliveira14@gmail.com>
2023-07-22 09:52:07 -0300

    finish main banner

--- src/app/pages/home/banner/banner.component.ts
+++ src/app/pages/home/banner/banner.component.ts
@@ -8,11 +8,10 @@ import { Component, OnInit } from "@angular/core";
 export class BannerComponent implements OnInit {
   banners = [
     "assets/img/banner-header.png",
-    "assets/img/banner-header.png",
-    "assets/img/banner-header.png",
+    "https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1614680376739-414d95ff43df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
   ];
 
-  bannerTranslatePos: number = 0;
   bannerIndex: number = 0;
 
   ngOnInit(): void {
@@ -22,21 +21,39 @@ export class BannerComponent implements OnInit {
   onCarouselInit(){
     setInterval(() => {
       this.bannerIndex ++
-      this.bannerTranslatePos+= 100;
 
       if(this.bannerIndex >= this.banners.length){
         this.bannerIndex = 0
-        this.bannerTranslatePos = 0;
       }
 
     }, 4000)
   }
 
   nextSlideBanner(){
+    this.bannerIndex++
 
+    if(this.bannerIndex >= this.banners.length){
+      this.bannerIndex = 0;
+    }
   }
 
   previousSlideBanner(){
+    this.bannerIndex--
 
+    if(this.bannerIndex < 0){
+      this.bannerIndex = this.banners.length - 1
+    }
+  }
+
+
+  onChangeImage(): string{
+    if(this.bannerIndex == 0){
+      return `translateX(0)`
+    } else if(this.bannerIndex == 1){
+      return `translateX(-100%)`
+    } else if(this.bannerIndex == 2){
+      return `translateX(-200%)`
+    }
+    return `translateX(0)`
   }
 }
